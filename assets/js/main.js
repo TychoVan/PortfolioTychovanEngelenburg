3814e96025ad29c287d9759471c604ac6d1b23bb
TychoVan <TychovanEngelenburg@gmail.com>
2022-11-10 15:29:49 +0100

    WIP CodeSnippets

--- assets/js/main.js
+++ assets/js/main.js
@@ -247,4 +247,30 @@
     })
   });
 
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  console.log(document.getElementsByClassName("codeSnippet"))
+  var img = document.getElementsByClassName("codeSnippet");
+
+  img.onclick = function(){
+    console.log("test")
+    var clickedImg = this.src
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+
+    modal.style.display = "block";
+    modalImg.src = clickedImg.src;
+    captionText.innerHTML = clickedImg.alt;
+  }
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+  
 })()
